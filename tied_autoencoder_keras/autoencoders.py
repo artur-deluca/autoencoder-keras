cef6740875243329563b7e2b0307a8b822a98002
Artur <artur_deluca@hotmail.com>
2019-12-23 13:19:02 +0100

    updated functions

--- tied_autoencoder_keras/autoencoders.py
+++ tied_autoencoder_keras/autoencoders.py
@@ -1,8 +1,6 @@
-from keras import backend as K
-from keras.engine import InputSpec
-from keras.layers import Dense, Dropout
-from sparsely_connected_keras import Sparse
-from keras.engine.topology import Layer
+from tensorflow.keras import backend as K
+from tensorflow.python.keras.layers import InputSpec, Layer
+from tensorflow.keras.layers import Dense, Dropout
 
 
 class DenseLayerAutoencoder(Dense):
@@ -114,48 +112,3 @@ class DenseLayerAutoencoder(Dense):
     @classmethod
     def from_config(cls, config):
         return cls(**config)
-
-
-class SparseLayerAutoencoder(Sparse):
-    def compute_output_shape(self, input_shape):
-        return input_shape
-
-    def build(self, input_shape):
-        if self.use_bias:
-            # bias2 is for after the latent layer
-            self.bias2 = self.bias = self.add_weight(
-                shape=(
-                    self.adjacency_mat.shape[0],
-                ),
-                initializer=self.bias_initializer,
-                name='bias2',
-                regularizer=self.bias_regularizer,
-                constraint=self.bias_constraint)
-        else:
-            self.bias2 = None
-        super().build(input_shape)
-
-    def call(self, inputs):
-        latent = self.kernel * self.adjacency_tensor
-        latent = K.dot(inputs, latent)
-        if self.use_bias:
-            latent = K.bias_add(latent, self.bias)
-        if self.activation is not None:
-            latent = self.activation(latent)
-        reconstruction = K.transpose(
-            self.kernel) * K.transpose(self.adjacency_tensor)
-        reconstruction = K.dot(latent, reconstruction)
-        if self.use_bias:
-            reconstruction = K.bias_add(reconstruction, self.bias2)
-        if self.activation is not None:
-            reconstruction = self.activation(reconstruction)
-        return reconstruction
-
-    def encode(self, inputs):
-        latent = self.kernel * self.adjacency_tensor
-        latent = K.dot(inputs, latent)
-        if self.use_bias:
-            latent = K.bias_add(latent, self.bias)
-        if self.activation is not None:
-            latent = self.activation(latent)
-        return latent
